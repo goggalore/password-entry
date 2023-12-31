efa5df0a35603cbee09a42587dd8d647def0118e
June Crane <june.crane@geogebra.at>
2023-12-30 18:11:12 -0700

    update eslint rules
    - extend 'plugin:react/recommended' & 'plugin:react/jsx-runtime'
    - add some custom rules for prettiness (and better legibility)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +16,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'indent': ['error', 2],
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    'object-curly-spacing': [ 'error', 'always', { 'objectsInObjects': false }],
+    'array-bracket-spacing': [ 'error', 'always', { 'objectsInArrays': false, 'arraysInArrays': false }],
+    'semi': ['error', 'never'],
+    'no-unexpected-multiline': ['error']
   },
 }
