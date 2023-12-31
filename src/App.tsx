6cd9c0507a411a9baa014125613a71f7c9322e88
June Crane <june.crane@geogebra.at>
2023-12-30 17:56:14 -0700

    initial commit
    - contains TypeScript React project initialized with vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="spin logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Password Validator</h1>
+    </>
+  )
+}
+
+export default App
